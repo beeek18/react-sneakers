1032e535e1ac9473deffaba45681c8280e89c8f4
Bekzodbek <kodirov2000@list.ru>
2023-02-10 11:25:11 +0300

    revert

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
-import AppContext from './context';
 
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
-import Orders from './pages/Orders';
 
 function App() {
   const [items, setItems] = React.useState([]);
@@ -15,82 +13,39 @@ function App() {
   const [favorites, setFavorites] = React.useState([]);
   const [searchValue, setSearchValue] = React.useState('');
   const [cartOpened, setCartOpened] = React.useState(false);
-  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
-    async function fetchData() {
-      try {
-        const [cartResponse, favoritesResponse, itemsResponse] = await Promise.all([
-          axios.get('https://63c590a5f3a73b347859655a.mockapi.io/cart'),
-          axios.get('https://63c590a5f3a73b347859655a.mockapi.io/favorites'),
-          axios.get('https://63c590a5f3a73b347859655a.mockapi.io/items'),
-        ]);
-
-        setIsLoading(false);
-        setCartItems(cartResponse.data);
-        setFavorites(favoritesResponse.data);
-        setItems(itemsResponse.data);
-      } catch (error) {
-        alert('Ошибка при запросе данных ;(');
-        console.error(error);
-      }
-    }
-
-    fetchData();
+    axios.get('https://63c590a5f3a73b347859655a.mockapi.io/items').then((res) => {
+      setItems(res.data);
+    });
+    axios.get('https://63c590a5f3a73b347859655a.mockapi.io/cart').then((res) => {
+      setCartItems(res.data);
+    });
+    axios.get('https://63c590a5f3a73b347859655a.mockapi.io/favorites').then((res) => {
+      setFavorites(res.data);
+    });
   }, []);
 
-  const onAddToCart = async (obj) => {
-    try {
-      const findItem = cartItems.find((item) => Number(item.parentId) === Number(obj.id));
-      if (findItem) {
-        setCartItems((prev) => prev.filter((item) => Number(item.parentId) !== Number(obj.id)));
-        await axios.delete(`https://63c590a5f3a73b347859655a.mockapi.io/cart/${findItem.id}`);
-      } else {
-        setCartItems((prev) => [...prev, obj]);
-        const { data } = await axios.post('https://63c590a5f3a73b347859655a.mockapi.io/cart', obj);
-        setCartItems((prev) =>
-          prev.map((item) => {
-            if (item.parentId === data.parentId) {
-              return {
-                ...item,
-                id: data.id,
-              };
-            }
-            return item;
-          }),
-        );
-      }
-    } catch (error) {
-      alert('Ошибка при добавлении в корзину');
-      console.error(error);
-    }
+  const onAddToCart = (obj) => {
+    axios.post('https://63c590a5f3a73b347859655a.mockapi.io/cart', obj);
+    setCartItems((prev) => [...prev, obj]);
   };
 
   const onRemoveItem = (id) => {
-    try {
-      axios.delete(`https://63c590a5f3a73b347859655a.mockapi.io/cart/${id}`);
-      setCartItems((prev) => prev.filter((item) => Number(item.id) !== Number(id)));
-    } catch (error) {
-      alert('Ошибка при удалении из корзины');
-      console.error(error);
-    }
+    axios.delete(`https://63c590a5f3a73b347859655a.mockapi.io/cart/${id}`);
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
   };
 
   const onAddToFavorite = async (obj) => {
     try {
-      if (favorites.find((favObj) => Number(favObj.id) === Number(obj.id))) {
+      if (favorites.find((favObj) => favObj.id === obj.id)) {
         axios.delete(`https://63c590a5f3a73b347859655a.mockapi.io/favorites/${obj.id}`);
-        setFavorites((prev) => prev.filter((item) => Number(item.id) !== Number(obj.id)));
       } else {
-        const { data } = await axios.post(
-          'https://63c590a5f3a73b347859655a.mockapi.io/favorites',
-          obj,
-        );
+        const { data } = await axios.post('https://63c590a5f3a73b347859655a.mockapi.io/favorites', obj);
         setFavorites((prev) => [...prev, data]);
       }
     } catch (error) {
       alert('Не удалось добавить в фавориты');
-      console.error(error);
     }
   };
 
@@ -98,54 +53,26 @@ function App() {
     setSearchValue(event.target.value);
   };
 
-  const isItemAdded = (id) => {
-    return cartItems.some((obj) => Number(obj.parentId) === Number(id));
-  };
-
   return (
-    <AppContext.Provider
-      value={{
-        items,
-        cartItems,
-        favorites,
-        isItemAdded,
-        onAddToFavorite,
-        onAddToCart,
-        setCartOpened,
-        setCartItems,
-      }}>
-      <div className="wrapper clear">
-        <Drawer
-          items={cartItems}
-          onClose={() => setCartOpened(false)}
-          onRemove={onRemoveItem}
-          opened={cartOpened}
+    <div className="wrapper clear">
+      {cartOpened && (
+        <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />
+      )}
+
+      <Header onClickCart={() => setCartOpened(true)} />
+
+      <Routes>
+        <Route path="/" element={<Home
+          items={items}
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+          onChangeSearchInput={onChangeSearchInput}
+          onAddToFavorite={onAddToFavorite}
+          onAddToCart={onAddToCart} />}
         />
-
-        <Header onClickCart={() => setCartOpened(true)} />
-
-        <Route path="" exact>
-          <Home
-            items={items}
-            cartItems={cartItems}
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-            onChangeSearchInput={onChangeSearchInput}
-            onAddToFavorite={onAddToFavorite}
-            onAddToCart={onAddToCart}
-            isLoading={isLoading}
-          />
-        </Route>
-
-        <Route path="favorites" exact>
-          <Favorites />
-        </Route>
-
-        <Route path="orders" exact>
-          <Orders />
-        </Route>
-      </div>
-    </AppContext.Provider>
+        <Route path="/favorites" element={<Favorites items={favorites} onAddToFavorite={onAddToFavorite} />} />
+      </Routes>
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 
 import './index.scss';
@@ -10,7 +10,9 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Routes>
+        <Route path='*' element={<App />} />
+      </Routes>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
