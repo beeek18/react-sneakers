10d18d0af3b1f09531b13e745ce1011fe97e9efb
Bekzodbek <kodirov2000@list.ru>
2023-03-13 15:43:51 +0300

    3

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import styles from './Card.module.scss';
 
-const Card = () => {
+console.log(styles);
+
+function Card(props) {
   return (
-    <div>index</div>
-  )
+    <div className={styles.card}>
+      <div className={styles.favorite}>
+        <img src="/img/unliked.svg" alt="Unliked" />
+      </div>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>{props.price} руб.</b>
+        </div>
+        <button className="button" onClick={props.onClick}>
+          <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default index
+export default Card;
--- src/hooks/useCart.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const useCart = () => {
-  return (
-    <div>useCart</div>
-  )
-}
