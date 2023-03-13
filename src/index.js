decd927a802dfab90e3882640af1aa1fa5209826
Bekzodbek <kodirov2000@list.ru>
2023-03-13 14:27:21 +0300

    1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import './index.scss';
+import 'macro-css';
 
 const rootElem = document.getElementById("root");
 
